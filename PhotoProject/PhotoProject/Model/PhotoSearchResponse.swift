553e44b3c622b86d47102abd8c278b8d5cd7338b
WaterBean <gkstnqls9@gmail.com>
2025-01-20 15:41:42 +0900

    [Refactor] 응답 별 모델 분리

--- PhotoProject/PhotoProject/Model/PhotoSearchResponse.swift
+++ PhotoProject/PhotoProject/Model/PhotoSearchResponse.swift
@@ -49,23 +49,3 @@ struct ImageUrl: Decodable {
     let small: String
 }
 
-
-struct PhotoStatisticsResponse: Decodable {
-    let id: String
-    let downloads: Statistics
-    let views: Statistics
-}
-
-struct Statistics: Decodable {
-    let total: Int
-    let historical: Historical
-}
-
-struct Historical: Decodable {
-    let values: [Value]
-}
-
-struct Value: Decodable {
-    let date: String
-    let value: Int
-}
---
+++ PhotoProject/PhotoProject/Model/PhotoStatisticsResponse.swift
@@ -0,0 +1,27 @@
+//
+//  PhotoStatisticsResponse.swift
+//  PhotoProject
+//
+//  Created by 한수빈 on 1/20/25.
+//
+
+
+struct PhotoStatisticsResponse: Decodable {
+    let id: String
+    let downloads: Statistics
+    let views: Statistics
+}
+
+struct Statistics: Decodable {
+    let total: Int
+    let historical: Historical
+}
+
+struct Historical: Decodable {
+    let values: [Value]
+}
+
+struct Value: Decodable {
+    let date: String
+    let value: Int
+}
